cbde9017bd9ff591657f211f66db322af40db098
Loperisum <jamjokercs@gmail.com>
2023-10-28 09:13:48 +0900

    asdf

---
+++ src/game/define.py
@@ -0,0 +1,15 @@
+import pygame
+
+WIDTH = 800
+HEIGHT = WIDTH + (WIDTH / 2)
+ROWS, COLS = 8, 8
+BLOCK_SIZE = WIDTH // COLS
+
+# rgb
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (128,128,128)
+
+CROWN = pygame.transform.scale(pygame.image.load('img/crown.png'), (44, 25))
---
+++ src/main.py
@@ -0,0 +1,13 @@
+import pygame
+import game.define
+
+FPS = 60
+
+pygame.display.set_caption('즐겁고 신나는 체커 게임\(￣︶￣*\))')
+
+def main():
+    # processing = 1
+    processing = True
+    clock = pygame.time.Clock()
+    
+main()
