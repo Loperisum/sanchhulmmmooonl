7f90a9fd2f66fde1ba54a367d4f6360f798be339
canbesad <jamjokercs@gmail.com>
2023-10-28 14:43:25 +0900

    Update board.py

--- src/game/board.py
+++ src/game/board.py
@@ -5,5 +5,8 @@ from .horse import Horse
 class Board:
     def __init__(self):
         self.red = self.black = 12
-        self.red_king = self.black_king = 12
-        
+        self.red_king = self.black_king = 0
+        self.create_board()
+    
+    def create_board(self):
+
